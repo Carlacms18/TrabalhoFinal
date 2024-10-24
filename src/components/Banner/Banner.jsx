74cd7504e31b038dc6e0d5b130a8727332a598f4
Carla Santos <carlacms18@gmail.com>
2024-10-24 17:56:04 -0100

    change cover photo and add text

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+
+const Banner = () => (
+    <div
+      className="h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('/Porton.jpg')" }}
+    >
+      <div className="text-center">
+        <h1 className="text-6xl font-extrabold text-white">FEEL THE MINDEL BREEZE</h1>
+      </div>
+    </div>
+  );
+  
+  export default Banner;
+  
